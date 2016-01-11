94fa8e57f11c119c4e2e8f99c558e2493b539b23
Michael Cavalea <callmecavs@gmail.com>
2016-01-10 20:38:03 -0500

    lock deps, fix render syntax

--- lib/client/index.jsx
+++ lib/client/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory, Link } from 'react-router'
+import { Router, Route, Link, browserHistory } from 'react-router'
 
-ReactDOM.render(
-  <Router history={browserHistory}>
+ReactDOM.render((
+  <Router history={ browserHistory }>
+
   </Router>
-, document.getElementById('root'))
+), document.getElementById('root'))
