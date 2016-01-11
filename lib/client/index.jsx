f6c8b21a7559c3eb54d16e3dcc2b189938ab5bee
Michael Cavalea <callmecavs@gmail.com>
2016-01-10 20:53:15 -0500

    add / route, main App component skeleton

--- lib/client/index.jsx
+++ lib/client/index.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, Link, browserHistory } from 'react-router'
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>outset-react</h1>
+    )
+  }
+}
+
 ReactDOM.render((
   <Router history={ browserHistory }>
-
+    <Route path="/" component={ App }/>
   </Router>
 ), document.getElementById('root'))
